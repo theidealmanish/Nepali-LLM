2e02a367590ae72f7d20ffa202676a2b9b016e85
Samir Wagle <98935307+SamirWagle@users.noreply.github.com>
2024-10-05 16:50:04 +0545

    corrected the code and extracted only necessary paragraph

--- scraper/scrapper2.py
+++ scraper/scrapper2.py
@@ -8,7 +8,9 @@ import csv
 service = Service(EdgeChromiumDriverManager().install())
 driver = webdriver.Edge(service=service)
 
-url = "https://ne.wikipedia.org/wiki/%E0%A4%85_%E0%A4%AC%E0%A5%8D%E0%A4%B0%E0%A4%BF%E0%A4%AB_%E0%A4%B9%E0%A4%BF%E0%A4%B8%E0%A5%8D%E0%A4%9F%E0%A5%8D%E0%A4%B0%E0%A5%80_%E0%A4%85%E0%A4%AB_%E0%A4%9F%E0%A4%BE%E0%A4%87%E0%A4%AE"  # Replace with the website you want to scrape
+
+#url = "https://ne.wikipedia.org/wiki/%E0%A4%85_%E0%A4%AC%E0%A5%8D%E0%A4%B0%E0%A4%BF%E0%A4%AB_%E0%A4%B9%E0%A4%BF%E0%A4%B8%E0%A5%8D%E0%A4%9F%E0%A5%8D%E0%A4%B0%E0%A5%80_%E0%A4%85%E0%A4%AB_%E0%A4%9F%E0%A4%BE%E0%A4%87%E0%A4%AE"  # Replace with the website you want to scrape
+url = "https://ne.wikipedia.org/wiki/%E0%A4%AA%E0%A5%8D%E0%A4%B0%E0%A4%BE%E0%A4%A3%E0%A4%B5%E0%A4%BE%E0%A4%AF%E0%A5%81"
 driver.get(url)
 
 
@@ -16,7 +18,7 @@ driver.get(url)
 p_tags = driver.find_elements(By.TAG_NAME, 'p')
 
 # Open CSV file to write the data
-with open('All Scraoed Data.csv', mode='w', newline='', encoding='utf-8') as file:
+with open('All Scraoed Data.csv', mode='a', newline='', encoding='utf-8') as file:
     writer = csv.writer(file)
     writer.writerow([ 'Text Content'])  # Header row
 
